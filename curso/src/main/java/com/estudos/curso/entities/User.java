fe45a036f6788c3b0205e241b19087a586b1daf3
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2025-05-24 11:23:30 -0300

    pedidos pt1

--- curso/src/main/java/com/estudos/curso/entities/User.java
+++ curso/src/main/java/com/estudos/curso/entities/User.java
@@ -1,12 +1,15 @@
 package com.estudos.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,8 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> order = new ArrayList<>();
 	
 	public User() {
 	}
@@ -73,6 +78,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrder() {
+		return order;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,6 +99,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
